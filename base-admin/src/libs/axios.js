84446158f9afffb20affcfb3c31f061273a5974d
柳倩倩 <928776712@qq.com>
2020-11-26 14:16:05 +0800

    后台管理系统

---
+++ base-admin/src/libs/axios.js
@@ -0,0 +1,112 @@
+import axios from 'axios'
+import store from '@/store'
+const Qs = require('qs');
+import { data } from 'autoprefixer'
+import { Message } from 'iview'
+// import { Spin } from 'iview'
+const addErrorLog = errorInfo => {
+  const { statusText, status, request: { responseURL } } = errorInfo
+  let info = {
+    type: 'ajax',
+    code: status,
+    mes: statusText,
+    url: responseURL
+  }
+  if (!responseURL.includes('saveErrorLogger')) store.dispatch('addErrorLog', info)
+}
+
+class HttpRequest {
+  constructor (baseUrl = baseURL,proxyPrefix = proxyPrefix) {
+    this.baseUrl = baseUrl
+    this.proxyPrefix=proxyPrefix
+    this.queue = {}
+  }
+  getInsideConfig (options) {
+    const config = {
+      baseURL: this.baseUrl,
+      headers: {
+        //
+      }
+    }
+    //参数中是普通表单
+    if(options.data&&options.headers&&options.headers["Content-Type"]&&options.headers["Content-Type"].indexOf('x-www-form-urlencoded')!=-1){
+      options.data=Qs.stringify(options.data);
+      console.log(options.data);
+    }
+
+    return config
+  }
+  destroy (url) {
+    delete this.queue[url]
+    if (!Object.keys(this.queue).length) {
+      // Spin.hide()
+    }
+  }
+  //请求响应200 之后的默认处理
+  successHandler(res){
+    console.log(res);
+  }
+
+  interceptors (instance, url) {
+    console.log(url);
+    // 请求拦截
+    instance.interceptors.request.use(config => {
+      // 添加全局的loading...
+      if (!Object.keys(this.queue).length) {
+        // Spin.show() // 不建议开启，因为界面不友好
+      }
+      this.queue[url] = true
+      return config
+    }, error => {
+      return Promise.reject(error)
+    })
+    // 响应拦截
+    instance.interceptors.response.use(res => {
+      this.destroy(url)
+      const { data, status } = res
+
+      this.successHandler(res);
+
+      return { data, status }
+    }, error => {
+      console.log("出错啦",error,url);
+      this.destroy(url)
+      let errorInfo = error.response
+      if (!errorInfo) {
+        const { request: { statusText, status }, config } = JSON.parse(JSON.stringify(error))
+        errorInfo = {
+          statusText,
+          status,
+          request: { responseURL: config.url }
+        }
+      }
+      this.dealError(errorInfo)//处理异常信息
+      addErrorLog(errorInfo)
+      return Promise.reject(error)
+    })
+  }
+  request (options) {
+    let url=options.url;
+    //增加代理前缀
+    let mind="";
+    if(url.length>1&&(url.indexOf("/")>0||url.indexOf("/")==-1)){
+      mind="/";
+    }
+    options.url=(this.proxyPrefix?this.proxyPrefix+mind:mind)+url;//重构url
+
+    const instance = axios.create()
+    options = Object.assign(this.getInsideConfig(options), options);
+    console.log(options);
+    this.interceptors(instance, options.url)
+    return instance(options)
+  }
+  //只处理请求码错误的请求
+  dealError (errorInfo){
+    if(!errorInfo.config.url.includes('saveErrorLogger')){
+      if(errorInfo.data){
+        Message.error(errorInfo.data.msg);
+      }
+    }
+  }
+}
+export default HttpRequest
---
+++ base-admin/vue.config.js
@@ -0,0 +1,57 @@
+const path = require('path')
+
+const resolve = dir => {
+  return path.join(__dirname, dir)
+}
+
+// 项目部署基础
+// 默认情况下，我们假设你的应用将被部署在域的根目录下,
+// 例如：https://www.my-app.com/
+// 默认：'/'
+// 如果您的应用程序部署在子路径中，则需要在这指定子路径
+// 例如：https://www.foobar.com/my-app/
+// 需要将它改为'/my-app/'
+// iview-admin线上演示打包路径： https://file.iviewui.com/admin-dist/
+const BASE_URL = process.env.NODE_ENV === 'production'
+  ? '/'
+  : '/'
+
+module.exports = {
+  // Project deployment base
+  // By default we assume your app will be deployed at the root of a domain,
+  // e.g. https://www.my-app.com/
+  // If your app is deployed at a sub-path, you will need to specify that
+  // sub-path here. For example, if your app is deployed at
+  // https://www.foobar.com/my-app/
+  // then change this to '/my-app/'
+  baseUrl: BASE_URL,
+  // tweak internal webpack configuration.
+  // see https://github.com/vuejs/vue-cli/blob/dev/docs/webpack.md
+  // 如果你不需要使用eslint，把lintOnSave设为false即可
+  lintOnSave: false,
+  chainWebpack: config => {
+    config.resolve.alias
+      .set('@', resolve('src')) // key,value自行定义，比如.set('@@', resolve('src/components'))
+      .set('_c', resolve('src/components'))
+  },
+  // 设为false打包时不生成.map文件
+  productionSourceMap: false
+  // 这里写你调用接口的基础路径，来解决跨域，如果设置了代理，那你本地开发环境的axios的baseUrl要写为 '' ，即空字符串
+  ,devServer: {
+    //host:'127.0.0.1',
+   // port: 9000,
+    //proxy:{'/api':{}},代理器中设置/api,项目中请求路径为/api的替换为target
+    proxy:{
+      '/api':{
+          target: 'http://127.0.0.1:9000',//代理地址，这里设置的地址会代替axios中设置的baseURL
+          changeOrigin: true,// 如果接口跨域，需要进行这个参数配置
+          //ws: true, // proxy websockets
+          //pathRewrite方法重写url
+          pathRewrite: {
+              '^/api': '' 
+              //pathRewrite: {'^/api': '/'} 重写之后url为 http://192.168.1.16:8085/xxxx
+              //pathRewrite: {'^/api': '/api'} 重写之后url为 http://192.168.1.16:8085/api/xxxx
+         }
+  }}
+  }
+}
